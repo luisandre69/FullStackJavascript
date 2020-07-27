4cd4c656c3cd4bddaccfb962be8f2bdd823c29cc
luisandre69 <luisandre_69@hotmail.com>
2020-07-27 16:06:32 +0100

    . React life-cycle

--- demo/src/Application.js
+++ demo/src/Application.js
@@ -3,6 +3,34 @@ import React, {
 } from 'react';
 
 class Application extends Component {
+
+    constructor(props){
+        super(props);
+
+    }
+
+    componentWillMount(props, state){
+
+    }
+
+    componentDidMount(props, state){
+        console.log("Mounted With", props, state)
+    }
+
+    componentWillReceiveProps(props){
+
+    }
+
+    componentWillUpdate(props,state){
+        if(this.props.name !== props.name){
+            //do something
+        }
+    }
+
+    componentDidUpdate(props,state){
+
+    }
+    
     render() {
         let name = "Luis";
         return ( 
